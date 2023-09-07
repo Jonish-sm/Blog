9dca5c9aa6ba8f071fb030ac48dce2319ac2fa4f
Jonish-sm <jonishsubba@gmail.com>
2023-09-07 15:25:11 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -3,13 +3,34 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    const filename = Date.now() + file.originalname;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
 app.use("/api/posts", postRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-      <img src={Logo} alt="" />
+      <Link className="link" to="/">Blog</Link>
       <span>
         Made with <b>React js</b>
       </span>
